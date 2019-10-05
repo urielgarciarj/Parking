a21aaac295a1ef91f26c831a1131a0a0cb3e61a0
Uriel <urielgarciarj@outlook.com>
2019-10-04 20:44:28 -0500

    Funciones asyncronas y timer agregados para la desaparicion de los tickets y el no permitir la entrada de nuevos tickets sin que haya pasado determinado tiempo

--- Parking.cs
+++ Parking.cs
@@ -21,24 +21,40 @@ namespace Parking
 
         public static class GlobalData
         {
-            public static int numTicket = 1;
+            public static int numTicket = 0;
+            public static Boolean flag = true;
         }
 
         private void BtnTicket_Click(object sender, EventArgs e)
         {
-            lblTicket.Show();
-            lblTicket.Text = GlobalData.numTicket.ToString();
-            ListViewItem lista = new ListViewItem(lblTicket.Text); //Agrega el numero de ticket
-            lista.SubItems.Add(lblTicket.Text); //Agrega la hora de entrada
-            lvParking.Items.Add(lista); //Le mando mi objeto, los datos a la listView
-            this.timer1.Start();
-            GlobalData.numTicket++;
+            if (GlobalData.flag == true)
+            {
+                agregarTicket();
+                lblTicket.Show();
+                lblTicket.Text = GlobalData.numTicket.ToString();
+                ListViewItem lista = new ListViewItem(lblTicket.Text); //Agrega el numero de ticket
+                lista.SubItems.Add(lblTicket.Text); //Agrega la hora de entrada
+                lvParking.Items.Add(lista); //Le mando mi objeto, los datos a la listView
+                this.timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("El tiempo de espera aun no termina");
+            }
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
             lblTicket.Text = GlobalData.numTicket.ToString();
             lblTicket.Hide();
+        }
+
+        static async void agregarTicket()
+        {
+            GlobalData.flag = false;
+            GlobalData.numTicket++;
+            await Task.Delay(5000);
+            GlobalData.flag = true;
         }
 
     }
